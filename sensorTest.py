c442845b5957e7b51b837840cbdef1fdac9f5762
James Stevenson <jpatstevenson@gmail.com>
2020-03-06 17:07:33 -0800

    added comments

--- sensorTest.py
+++ sensorTest.py
@@ -2,12 +2,15 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
+#set PIR motion sensor on 11th pin
+GPIO.setup(11, GPIO.IN)
 while True:
     i=GPIO.input(11)
-    if i==0:                 #When output from motion sensor is LOW
+    # sensor not detecting anything
+    if i==0:
         print("No intruders",i)
         time.sleep(1)
-    elif i==1:               #When output from motion sensor is HIGH
+    # sensor triggered
+    elif i==1:
         print("Intruder detected",i)
     time.sleep(1)
